95db9a2e1d0d7a854f49ad6b24930e51e736569e
novak <novakmzv@gmail.com>
2025-07-20 00:47:25 -0500

    refactor: modify StaffsController

--- app/Http/Controllers/Admin/Staff/StaffsController.php
+++ app/Http/Controllers/Admin/Staff/StaffsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Staff;
 
 use Carbon\Carbon;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
@@ -17,16 +18,13 @@ class StaffsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(Request $request): JsonResponse
     {
         $this->authorize('viewAny', User::class);
 
         $search = $request->search;
 
         $users = User::where(DB::raw("CONCAT(users.name,' ',IFNULL(users.surname,''),' ',users.email)"), "like", "%" . $search . "%")
-            // "name","like","%".$search."%"
-            // ->orWhere("surname","like","%".$search."%")
-            // ->orWhere("email","like","%".$search."%")
             ->orderBy("id", "desc")
             ->whereHas("roles", function ($q) {
                 $q->where("name", "not like", "%DOCTOR%");
@@ -38,7 +36,7 @@ class StaffsController extends Controller
         ]);
     }
 
-    public function config()
+    public function config(): JsonResponse
     {
         $roles = Role::where("name", "not like", "%DOCTOR%")->get();
 
@@ -49,7 +47,7 @@ class StaffsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         $this->authorize('create', User::class);
         $users_is_valid = User::where("email", $request->email)->first();
@@ -69,8 +67,7 @@ class StaffsController extends Controller
         if ($request->password) {
             $request->request->add(["password" => bcrypt($request->password)]);
         }
-        // "Fri Oct 08 1993 00:00:00 GMT-0500 (hora estándar de Perú)"
-        // Eliminar la parte de la zona horaria (GMT-0500 y entre paréntesis)
+
         $date_clean = preg_replace('/\(.*\)|[A-Z]{3}-\d{4}/', '', $request->birth_date);
 
         $request->request->add(["birth_date" => Carbon::parse($date_clean)->format("Y-m-d h:i:s")]);
@@ -87,7 +84,7 @@ class StaffsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
         $this->authorize('view', User::class);
         $user = User::findOrFail($id);
@@ -100,7 +97,7 @@ class StaffsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): JsonResponse
     {
         $this->authorize('update', User::class);
         $users_is_valid = User::where("id", "<>", $id)->where("email", $request->email)->first();
@@ -131,7 +128,6 @@ class StaffsController extends Controller
             $request->request->add(["birth_date" => Carbon::parse($date_clean)->format("Y-m-d h:i:s")]);
         }
 
-        // $request->request->add(["birth_date" => Carbon::parse($request->birth_date, 'GMT')->format("Y-m-d h:i:s")]);
         $user->update($request->all());
 
         if ($request->role_id && $request->role_id != $user->roles()->first()->id) {
@@ -149,7 +145,7 @@ class StaffsController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): JsonResponse
     {
         $this->authorize('delete', User::class);
         $user = User::findOrFail($id);
@@ -164,6 +160,4 @@ class StaffsController extends Controller
             "message" => 200
         ]);
     }
-
-    
 }
