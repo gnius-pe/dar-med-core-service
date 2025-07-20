c91f4c36d03bfb7582772118167849cc731087cb
novak <novakmzv@gmail.com>
2025-07-20 00:37:07 -0500

    refactor: modify user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\Doctor\Specialitie;
 use Spatie\Permission\Traits\HasRoles;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use App\Models\Doctor\DoctorScheduleDay;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -61,26 +60,23 @@ class User extends Authenticatable implements JWTSubject
      *
      * @return mixed
      */
-    public function getJWTIdentifier()
+    public function getJWTIdentifier(): mixed
     {
         return $this->getKey();
     }
- 
+
     /**
      * Return a key value array, containing any custom claims to be added to the JWT.
      *
      * @return array
      */
-    public function getJWTCustomClaims()
+    public function getJWTCustomClaims(): array
     {
         return [];
     }
 
-    public function specialitie() {
+    public function specialitie(): BelongsTo
+    {
         return $this->belongsTo(Specialitie::class);
-    }
-
-    public function schedule_days() {
-        return $this->hasMany(DoctorScheduleDay::class);
     }
 }
