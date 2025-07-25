704a3b2ffdb91f80c542ce07faf5d8c3d246086a
novak <novakmzv@gmail.com>
2025-07-25 18:45:44 -0500

    feat: create UserCollection

--- app/Http/Resources/User/UserCollection.php
+++ app/Http/Resources/User/UserCollection.php
@@ -15,7 +15,7 @@ class UserCollection extends ResourceCollection
     public function toArray(Request $request): array
     {
         return [
-            "data" => UserResource::collection($this->collection),
+            "users" => UserResource::collection($this->collection),
         ];
     }
 }
--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -15,68 +15,25 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $HOUR_SCHEDULES = collect([]);
-        $days_week = [
-            "Lunes" => "table-primary",
-            "Martes" => "table-secondary",
-            "Miercoles" => "table-success",
-            "Jueves" => "table-warning",
-            "Viernes" => "table-info",
-            "Sabado" => "table-light",
-            "Domingo" => "custom-sunday",
-        ];
-
-        $days_name = "";
-        foreach ($this->resource->schedule_days as $key => $schedule_day) {
-            $days_name .= ($schedule_day->day . "-");
-            foreach ($schedule_day->schedules_hours as $schedules_hour) {
-                $HOUR_SCHEDULES->push([
-                    "day" => [
-                        "day" => $schedule_day->day,
-                        "class" => $days_week[$schedule_day->day],
-                    ],
-                    "day_name" => $schedule_day->day,
-                    "hours_day" => [
-                        "hour" => $schedules_hour->doctor_schedule_hour->hour,
-                        "format_hour" => Carbon::parse(date("Y-m-d") . ' ' . $schedules_hour->doctor_schedule_hour->hour . ":00:00")->format("h:i A"),
-                        "items" => [],
-                    ],
-                    "hour" => $schedules_hour->doctor_schedule_hour->hour,
-                    "grupo" => "all",
-                    "item" => [
-                        "id" => $schedules_hour->doctor_schedule_hour->id,
-                        "hour_start" => $schedules_hour->doctor_schedule_hour->hour_start,
-                        "hour_end" => $schedules_hour->doctor_schedule_hour->hour_end,
-                        "format_hour_start" => Carbon::parse(date("Y-m-d") . ' ' . $schedules_hour->doctor_schedule_hour->hour_start)->format("h:i A"),
-                        "format_hour_end" => Carbon::parse(date("Y-m-d") . ' ' . $schedules_hour->doctor_schedule_hour->hour_end)->format("h:i A"),
-                        "hour" => $schedules_hour->doctor_schedule_hour->hour,
-                    ],
-                ]);
-            }
-        }
-
         return [
             "id" => $this->resource->id,
             "name" => $this->resource->name,
             "surname" => $this->resource->surname,
-            "full_name" => $this->resource->name . ' ' . $this->resource->surname,
+            "full_name" => $this->resource->name . ' ' . ($this->resource->surname ?? ''),
             "email" => $this->resource->email,
-            "birth_date" => $this->resource->birth_date ? Carbon::parse($this->resource->birth_date)->format("Y/m/d") : NULL,
+            "mobile" => $this->resource->mobile,
+            "avatar" => $this->resource->avatar ? env("APP_URL") . "storage/" . $this->resource->avatar : null,
+            "birth_date" => $this->resource->birth_date ? Carbon::parse($this->resource->birth_date)->format("Y-m-d") : null,
             "gender" => $this->resource->gender,
             "education" => $this->resource->education,
-            "designation" => $this->resource->designation,
             "address" => $this->resource->address,
-            "mobile" => $this->resource->mobile,
-            "created_at" => $this->resource->created_at->format("Y/m/d"),
-            "role" => $this->resource->roles->first(),
-            "specialitie_id" => $this->resource->specialitie_id,
-            "specialitie" => $this->resource->specialitie ? [
-                "id" => $this->resource->specialitie->id,
-                "name" => $this->resource->specialitie->name,
-            ] : NULL,
-            "avatar" => env("APP_URL") . "storage/" . $this->resource->avatar,
-            "schedule_selecteds" => $HOUR_SCHEDULES,
-            "days_name" => rtrim($days_name, "-"),
+            "role" => $this->resource->roles->first() ? [
+                "id" => $this->resource->roles->first()->id,
+                "name" => $this->resource->roles->first()->name,
+            ] : null,
+            "roles" => $this->resource->roles->pluck('name')->toArray(),
+            "permissions" => $this->resource->getAllPermissions()->pluck('name')->toArray(),
+            "created_at" => $this->resource->created_at->format("Y-m-d H:i:s"),
         ];
     }
 }
